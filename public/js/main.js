8bb064e0bd33783e5cc652fbc76398006ce35a9a
SimonWoodtli <xnasero@posteo.net>
2023-11-22 12:29:47 -0500

    ✨♻️  rm grid use flex, add sidebar mobile+desktop
    * Add mobile Sidebar, use popup navigation bar
    * Add desktop Sidebar, use sliding navigation bar
    * Rm grid layout to structure the whole page (on main container). Use
      flexbox instead. The reason being it was not working well with
      animations when you use JS to toggle columns 4 to 3 columns and vice
      versa the effect is immediatly, the same issue exists with using
      'hidden'. And Trying to be clever with the 'opacity' or 'width' will
      also have bad cascading effects that can be leveled out with JS, but
      are hacky. So hence the decision to use flexbox as the main layout.
    * Commented out a failed feature for the mobile sidebar. Which supposed
      to be able to close the sidebar when touching outside of the sidebar
      into the main content. However this only worked on PC with a minimized
      browser window, on the phone it did not. I might think of
      an alternative way of doing this in the future.
    
    Advice:
    
    * If I need to use a Sidebar or mobile navbar on a website I
      will always prefer using flexbox for that. Use grid to layout the main
      content, which comes after header but yeah not for the sidebars or
      toggable nav bars on mobile unless you don't want to add any
      animations.
    
    TODO:
    
    * Rm all comments about failed attempt to add mobile slidebar with
      touching outside sidebar to close it.
    * Finish styling the mobile and and desktop sidebar and add rest of
      content
    * Finish styling the header

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,19 @@
-// Dark mode
+// ----------------------------- Dark-Mode ----------------------------
 const darkModeToggle = document.querySelector('#toggleDark');
 const darkModeStateKey = "DarkMode";
 const isDark = JSON.parse(localStorage.getItem(darkModeStateKey) || 'false')
 
+// This slightly favours dark theme. Like overwriting of light happens if
+// system (matchMedia())is set dark. But if system is set light and the
+// user set dark mode previously on the site it stays dark. Makes
+// more sense IMO than having this complicated 3 options like the
+// tailwind docs has.
 if (isDark || window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.documentElement.classList.add('theme-dark');
+  // would be nice to have 'add' here for the icon switch. But only works
+  // with '.toggleDark:before {...}' because ':after' adds both icons :(
   darkModeToggle.classList.remove('toggleDark')
 }
-
 darkModeToggle.addEventListener('click', () => {
   if (document.documentElement.classList.contains('theme-dark')) {
     document.documentElement.classList.remove('theme-dark');
@@ -20,3 +26,75 @@ darkModeToggle.addEventListener('click', () => {
 
   }
 })
+// ------------------------------ Sidebar -----------------------------
+//SIDEBAR Mobile Issue:
+//If you want the sidebar to close not only if the user
+//clicks on the close btn, but anywhere on the screen (outside of the
+//sidebar). I tried to implement that into the same desktop sidebar. But
+//this won't work as we need inset-0 and give a mobile width *because my
+//desktop sidebar is setup to have a width for each screen size*. But at that
+//point the container no longer spans over whole canvas. And since the
+//closing button is a child it cannot span over whole canvas too, which
+//is required.
+//
+//1. I could try to add the btn outside of the sidebar and make that
+//work *challenge how to position it relative, if parent is no longer
+//sidebar* => failed
+//2. I could add a whole new sidebar just for mobile => current solution
+//3. I could use Go template lang to use the same sidebar twice but
+//adjust the classes where needed (IMO better than 2. but still a lot of
+//work)
+//4. I could use JS to solve the issue to just listen on clicks outside
+//the sidebar and act accordingly. (a little too nasty, fun idea but
+//unacceptable)
+//Currently went with 2. but I am not super happy as it added a lot of
+//complexity for a simple feature. I might skip this feature altogether
+//unless I can get rid of the JS hack I had to introduce. See
+//btnSidebarMobile event Listener
+//const isRetina = () => window.devicePixelRatio > 1;
+//TODO make an animation for closing/opening svg, right now just simple
+const btnSidebar = document.querySelector('#btnSidebar');
+const svgSidebar = document.querySelector('#svgSidebar');
+const svgSidebarPath = svgSidebar.querySelector('path');
+const sidebar = document.querySelector('#sidebar');
+
+const btnSidebarMobile = document.querySelector('#btnSidebarMobile');
+const svgSidebarMobile = document.querySelector('#svgSidebarMobile');
+const svgSidebarMobilePath = svgSidebarMobile.querySelector('path');
+const sidebarMobile = document.querySelector('#sidebarMobile');
+
+btnSidebarMobile.addEventListener('click', (e) => {
+  //toggle between hamburger and X:
+  svgSidebarMobilePath.setAttribute('d', svgSidebarMobilePath.getAttribute('d') === 'M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5' ? 'M6 18L18 6M6 6l12 12' : 'M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5');
+  //initiate closing/opening:
+  sidebarMobile.classList.toggle('-translate-x-full')
+
+  //Obsolete: Method that should have closed sidebar on mobile when
+  //touched outside sidebar (worked on PC with clicks but on phone it
+  //did not)
+
+  //btnSidebarMobile.classList.toggle('md:-right-[2.6rem]')
+  //btnSidebarMobile.classList.toggle('-right-[4.1rem]')
+  //btnSidebarMobile.classList.toggle('left-[17.6rem]')
+
+  //This is a bit of a hack to avoid the burger sliding all the way from
+  //the offcanvas right position to the canvas left position when
+  //sidebar is closing
+  //TODO find a better way to deal with this cosmetic issue, I tried to
+  //use 'transition-none' on the btn but that didn't work :(
+  //
+  //This issue only exists because of wanting the mobile sidebar to be
+  //closable while tapping anywhere outside of the sidebar. This feature
+  //added a lot of dept. As I had to create two seperate sidebars and
+  //this JS hack on top of it.
+  //btnSidebarMobile.classList.toggle('hidden')
+  //setTimeout(() => {btnSidebarMobile.classList.toggle('hidden')}, 200)
+})
+
+btnSidebar.addEventListener('click', (e) => {
+  //TODO try to solve this without the setTimout() func
+  sidebar.classList.toggle('xl:translate-x-0')
+  setTimeout(() => { sidebar.classList.toggle('xl:relative')}, 100 )
+  svgSidebarPath.setAttribute('d', svgSidebarPath.getAttribute('d') === 'M6 18L18 6M6 6l12 12' ? 'M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5' : 'M6 18L18 6M6 6l12 12');
+})
+
